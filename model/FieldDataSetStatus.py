d425b48eae6e5ab5a669c5d9a060679160501dd8
mmmaaaxxx77 <mmmaaaxxx77@gmail.com>
2016-02-05 13:59:37 +0800

    push code

---
+++ model/FieldDataSetStatus.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import uuid
+import datetime
+from peewee import Model, Field, CharField, DateField, TextField, DateTimeField, MySQLDatabase, IntegerField
+
+test = MySQLDatabase("", host="", port=3306, user="",
+                     passwd="")
+
+
+class FieldDataSetStatus(Model):
+    entity_type = CharField(max_length=128, primary_key=True, null=False)
+    bundle = CharField(max_length=128, null=False)
+    deleted = IntegerField(null=False)
+    entity_id = IntegerField(null=False, unique=True)
+    revision_id = IntegerField(unique=True)
+    language = CharField(max_length=32, null=False)
+    delta = IntegerField(null=False, unique=True)
+    field_dataset_status_value = CharField(max_length=255, null=True)
+
+    class Meta:
+        database = test
+        db_table = 'field_data_field_dataset_status'
